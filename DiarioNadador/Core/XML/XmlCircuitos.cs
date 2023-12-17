a74d21d5b47929a7fea2d1ffcfc4d3f1829c6791
Kingomac <kingomacyt@gmail.com>
2023-12-17 11:22:08 +0100

    Arreglar XML

--- DiarioNadador/Core/XML/XmlCircuitos.cs
+++ DiarioNadador/Core/XML/XmlCircuitos.cs
@@ -3,57 +3,56 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
-namespace Circuito
-{ 
- public class XmlCircuito
+namespace DiarioNadador.Core.XML;
+
+public class XmlCircuito
+{
+    private static readonly string RutaArchivoXml = "circuitos.xml";
+
+    public static void CircuitosToXml(double distancia, string lugar, string notas, string urlMapa)
     {
-        private static string RutaArchivoXml = "circuitos.xml";
-
-        public static void CircuitosToXml(double distancia, string lugar, string notas, string urlMapa)
+        try
         {
-            try
+            var nuevoCircuito = new Circuito(distancia, lugar, notas, urlMapa);
+
+            var listaCircuitos = XmlToCircuitos();
+
+            listaCircuitos.Add(nuevoCircuito);
+
+            var serializer = new XmlSerializer(typeof(List<Circuito>));
+
+            using (TextWriter writer = new StreamWriter(RutaArchivoXml))
             {
-                Circuito nuevoCircuito = new Circuito(distancia, lugar, notas, urlMapa);
-
-                List<Circuito> listaCircuitos = XmlToCircuitos();
-
-                listaCircuitos.Add(nuevoCircuito);
-
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Circuito>));
-
-                using (TextWriter writer = new StreamWriter(RutaArchivoXml))
-                {
-                    serializer.Serialize(writer, listaCircuitos);
-                }
-
-                Console.WriteLine($"Archivo XML guardado en: {Path.GetFullPath(RutaArchivoXml)}");
+                serializer.Serialize(writer, listaCircuitos);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error al crear el archivo XML: {ex.Message}");
-            }
+
+            Console.WriteLine($"Archivo XML guardado en: {Path.GetFullPath(RutaArchivoXml)}");
         }
-
-        public static List<Circuito> XmlToCircuitos()
+        catch (Exception ex)
         {
-            try
-            {
-                if (File.Exists(RutaArchivoXml))
-                {
-                    XmlSerializer serializer = new XmlSerializer(typeof(List<Circuito>));
+            Console.WriteLine($"Error al crear el archivo XML: {ex.Message}");
+        }
+    }
 
-                    using (FileStream fileStream = new FileStream(RutaArchivoXml, FileMode.Open))
-                    {
-                        return (List<Circuito>)serializer.Deserialize(fileStream);
-                    }
+    public static List<Circuito> XmlToCircuitos()
+    {
+        try
+        {
+            if (File.Exists(RutaArchivoXml))
+            {
+                var serializer = new XmlSerializer(typeof(List<Circuito>));
+
+                using (var fileStream = new FileStream(RutaArchivoXml, FileMode.Open))
+                {
+                    return (List<Circuito>)serializer.Deserialize(fileStream);
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error al cargar la lista de circuitos desde el archivo XML: {ex.Message}");
-            }
-
-            return new List<Circuito>();
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al cargar la lista de circuitos desde el archivo XML: {ex.Message}");
+        }
+
+        return new List<Circuito>();
     }
 }
